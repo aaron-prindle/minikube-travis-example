68188bce3f4d15a0b2c0eab18c71dd34fb075023
Aaron Prindle <aprindle@google.com>
2017-09-05 23:00:48 -0700

    fixed hellonode

--- hellonode/hellonode-test.sh
+++ hellonode/hellonode-test.sh
@@ -20,12 +20,4 @@ if [ "$HELLONODE_OUTPUT" != "HELLO" ]; then
   echo "TEST FAILURE: Unexpected output from hellonode service"
   exit 1
 fi
-echo "TEST SUCCESS: Expected output recieved from hellonode service"#!/usr/bin/env bash
-
-set -e
-HELLONODE_OUTPUT="$(curl $HELLONODE_URL)"
-if [ "$HELLONODE_OUTPUT" != "HELLO" ]; then
-  echo "TEST FAILURE: Unexpected output from hellonode service"
-  exit 1
-fi
 echo "TEST SUCCESS: Expected output recieved from hellonode service"
--- hellonode/hellonode-travis.sh
+++ hellonode/hellonode-travis.sh
@@ -19,6 +19,7 @@ docker build -t hellonode:v1 hellonode-app/
 ./minikube-ci-initialize.sh
 ./kubectl create -f hellonode-k8s-yaml/hellonode-rc.yaml
 ./kubectl create -f hellonode-k8s-yaml/hellonode-svc.yaml
-HELLONODE_URL="$(./minikube service --url --wait=300 --interval=2 hellonode 2>/dev/null)"
+HELLONODE_URL="$(./kubectl get svc hellonode -o go-template='{{ .spec.clusterIP }}{{ "\n" }}')":8080
+#HELLONODE_URL="$(./minikube service --url --wait=300 --interval=2 hellonode 2>/dev/null)"
 export HELLONODE_URL
 ./hellonode-test.sh
